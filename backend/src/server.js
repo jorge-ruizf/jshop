12fcc443555c9b49bb3d559cc33bfd25f129226a
Jorge Andrés Ruiz Flores <jorge.ruizf@udea.edu.co>
2026-04-26 14:28:52 -0500

    curl funcionando

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { app } from './app.js';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server listening on http://127.0.0.1:${PORT}`);
