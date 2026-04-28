4324d4ad2dbe76ef03df52ff12f10227a721f97a
Jorge Andrés Ruiz Flores <jorge.ruizf@udea.edu.co>
2026-04-27 22:16:08 -0500

    para vercel 10

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { apiRouter } from './routes/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFound } from './middlewares/notFound.js';
 
 const app = express();
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use(express.json());
 
