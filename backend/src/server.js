8f3a0b024e7007216b04e74b2bbe7e5bdd3467f2
Jorge Andrés Ruiz Flores <jorge.ruizf@udea.edu.co>
2026-04-27 20:42:06 -0500

    para render

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ import { app } from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
