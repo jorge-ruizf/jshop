25af88eb0fe7850709cd601b81f27ca81d3694b3
Jorge Andrés Ruiz Flores <jorge.ruizf@udea.edu.co>
2026-05-26 20:54:33 -0500

    cambio en user routes para que lea lo de su controller

--- backend/src/routes/usuario.routes.js
+++ backend/src/routes/usuario.routes.js
@@ -1,33 +1,38 @@
 import { Router } from 'express';
 import { requireFields, parseId } from '../middlewares/validateBody.js';
-import { listUsuarios, getUsuario, createUsuario, updateUsuario, deleteUsuario, } from '../controllers/usuario.controller.js';
-import { getUsuarioByCorreo } from '../controllers/usuario.controller.js';
+
+import {
+  listUsuarios,
+  getUsuario,
+  createUsuario,
+  updateUsuario,
+  deleteUsuario,
+  getUsuarioByCorreo
+} from '../controllers/usuario.controller.js';
 
 const usuarioRouter = Router();
 
 usuarioRouter.get('/', listUsuarios);
 
-usuarioRouter.get('/by-email', async (req, res, next) => {
-    try {
-        const { correo } = req.query;
-
-        const usuario = await prisma.usuario.findUnique({
-            where: { correo },
-        });
-
-        res.json(usuario ?? null);
-    } catch (err) {
-        next(err);
-    }
-});
+usuarioRouter.get('/by-email', getUsuarioByCorreo);
 
 usuarioRouter.get('/:id', parseId, getUsuario);
+
 usuarioRouter.post(
   '/',
-  requireFields(['nombre', 'usuario', 'correo', 'id_pais', 'id_rol', 'id_metodo_pago_fav']),
+  requireFields([
+    'nombre',
+    'usuario',
+    'correo',
+    'id_pais',
+    'id_rol',
+    'id_metodo_pago_fav'
+  ]),
   createUsuario,
 );
+
 usuarioRouter.put('/:id', parseId, updateUsuario);
+
 usuarioRouter.delete('/:id', parseId, deleteUsuario);
 
 export { usuarioRouter };
